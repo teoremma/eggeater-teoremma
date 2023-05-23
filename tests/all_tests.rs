f41531e012a706ec2d4d96af844c58ec8cc857be
emmanuel <emmanuelanaya@outlook.com>
2023-05-23 12:26:31 -0700

    Move tests to folder and rename to eggeater

--- tests/all_tests.rs
+++ tests/all_tests.rs
@@ -1,40 +1,24 @@
 mod infra;
-// mod cobra_own; 
-// mod cobra_grading;
+mod cobra_own; 
+mod cobra_grading;
+mod diamondback_own;
 
-// Your tests go here!
-success_tests! {
-    {
-        name: fact,
-        file: "fact.snek",
-        input: "10",
-        expected: "3628800",
-    },
-    {
-        name: even_odd_1,
-        file: "even_odd.snek",
-        input: "10",
-        expected: "10\ntrue\ntrue",
-    },
-    {
-        name: even_odd_2,
-        file: "even_odd.snek",
-        input: "9",
-        expected: "9\nfalse\nfalse",
-    },
-    {
-        name: sum4,
-        file: "sum4.snek",
-        expected: "1234",
-    }
-}
+// // Your tests go here!
+// success_tests! {
+//     {
+//         name: fact,
+//         file: "fact.snek",
+//         input: "10",
+//         expected: "3628800",
+//     },
+// }
 
-runtime_error_tests! {}
+// runtime_error_tests! {}
 
-static_error_tests! {
-    {
-        name: duplicate_params,
-        file: "duplicate_params.snek",
-        expected: "",
-    }
-}
+// static_error_tests! {
+//     {
+//         name: duplicate_params,
+//         file: "duplicate_params.snek",
+//         expected: "",
+//     }
+// }
